31884803df2f3cdbea887238d7b77374bc36bb06
Vipul Sharma <panditsfire@gmail.com>
2021-09-14 16:15:05 +0530

    initial commit

--- super-ui-ios/ViewController.swift
+++ super-ui-ios/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    let webView : WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        let webView = WKWebView(frame: .zero,
+                                configuration: configuration)
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        
+        guard let url = URL(string: "https://mysuper.link") else {
+        return }
+        webView.load(URLRequest(url: url))
+        webView.customUserAgent = "ipad/iphone/macbook"
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
 }
 
